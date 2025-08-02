1ce9036ef07822d260f8fb8915e4a93ebadcfbfc
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-08-02 14:46:57 +0300

    Configure cmp.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,13 +1,44 @@
 return {
   { 
-    -- "neovim/nvim-lspconfig",
-    -- dependencies = {
-    --   "hrsh7th/cmp-nvim-lsp",
-    --   'hrsh7th/cmp-buffer',
-    --   'hrsh7th/cmp-path',
-    --   'hrsh7th/cmp-cmdline',
-    --   'hrsh7th/cmp-vsnip',
-    --   'hrsh7th/vim-vsnip'
-    -- },
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip'
+    },
+    opts = function()  
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      local cmp = require("cmp")
+
+      return {
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+          { name = 'path' },
+        }, {
+          { name = 'buffer' },
+        })
+      }
+    end,
   },
 }
