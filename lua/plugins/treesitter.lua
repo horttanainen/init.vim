8b763a97c6b120b762e4c628db0b73b26c9e8ee3
Santeri Horttanainen <santeri.horttanainen@external.wolt.com>
2026-02-11 12:37:54 +0200

    Use treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,40 @@
- return {
-    {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      event = { "BufReadPost", "BufNewFile" },
-      config = function()
-        require("nvim-treesitter").setup({
-          ensure_installed = {
-            "c",
-            "clojure",
-            "go",
-            "kotlin",
-            "java",
-            "lua",
-            "vimdoc",
-            "typescript",
-            "javascript",
-            "zig",
-            "bash",
-            "html",
-            "css",
-            "json",
-            "markdown",
-            "python",
-            "swift"
-          },
-          auto_install = true,
-        })
-      end,
-    },
-  }
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter")
+
+      -- Install parsers for languages you use
+      ts.install({
+        "c",
+        "clojure",
+        "kotlin",
+        "java",
+        "lua",
+        "vimdoc",
+        "typescript",
+        "javascript",
+        "zig",
+        "bash",
+        "html",
+        "css",
+        "json",
+        "markdown",
+        "go",
+        "python",
+        "swift"
+      })
+
+      -- Enable treesitter highlighting and indentation for all filetypes
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("treesitter", { clear = true }),
+        callback = function(ev)
+          pcall(vim.treesitter.start, ev.buf)
+          vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+}
